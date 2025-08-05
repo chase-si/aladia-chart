df90e3582aaebaad339180c5934d79bde4a49329
chase <chase_si@163.com>
2025-08-05 10:56:02 +0800

    [fix]fix the dir path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '',
+  basePath: '/aladia-chart',
+  assetPrefix: '/aladia-chart/',
   images: {
     unoptimized: true
   }
